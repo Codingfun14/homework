ceb0bdb43b733967f5e0933d339282932af81cc8
Matthew Harris <harrism@wlhs.wlwv.k12.or.us>
2024-11-07 06:39:17 +0000

    done

---
+++ madlibsproj1.py
@@ -0,0 +1,40 @@
+print("welcome to a madlibs game")
+
+name = input("what is your name?")
+month = input("what month is it? ")
+day = input ("What day of the month is it (1-31)? ")
+year =input("what is the year currently? ")
+
+
+
+noun_1 = input("Now lets start off by picking your first noun: ")
+noun_2 = input("How bout a second: ")
+noun_3 = input("How bout a third: ")
+noun_4 = input("How bout a fourth: ")
+noun_5 = input("How bout a fifth: ")
+
+verb_1 = input("Now lets pick your first verb: ")
+verb_2 = input("how about a second: ")
+verb_3 = input("how about a third: ")
+verb_4 = input("how about a fourth: ")
+verb_5 = input("how about a fifth: ")
+
+adjective_1 = input("Now lets pick your first adjective: ")
+adjective_2 = input("How about a second: ")
+adjective_3 = input("How about a third: ")
+
+adverb_1 = input("Now lets pick your fist adverb: ")
+adverb_2 = input("How about a second: ")
+
+print(f"Now lets write you a story")
+
+   
+print(f"{name}")
+print(f"{month}")
+print(f"{day}")
+print(f"{year}")
+print(f"Once upon a time, there was a {noun_1} who loved to {verb_1} with their {adjective_1} {noun_2}.\
+    {name} would {verb_2} {adverb_1} while their {noun_3} {verb_3} nearby.\
+    One day, they found a {adjective_2} {noun_4} that could {verb_4} and decided to {verb_5} it.\
+    It turned out to be a {noun_5} which made it a {adjective_3} adventure, leaving everyone {adverb_2} amazed.")
+     
